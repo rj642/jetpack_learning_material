0557def4e31d163e73b5692f1b0375c66851e7c2
Rahul Jha <rahulj.rj642@gmail.com>
2023-05-09 13:15:53 +0530

    update: documenting it

--- app/src/main/java/com/example/jetpackexample/navigation_impl/BottomNavItem.kt
+++ app/src/main/java/com/example/jetpackexample/navigation_impl/BottomNavItem.kt
@@ -2,8 +2,18 @@ package com.example.jetpackexample.navigation_impl
 
 import com.example.jetpackexample.R
 
+/**
+ * Sealed class is created to have all the child item so that none of the cases is missed
+ */
+
+/**
+ * This class consists of title along with icon and screenRouter name
+ */
 sealed class BottomNavItem(var title: String, var icon: Int, var screenRoute: String) {
 
+    /**
+     * Assume these objects as a menu item just like how we create in standard xml
+     */
     object Home: BottomNavItem("Home", R.drawable.ic_home, "home")
     object MyNetwork: BottomNavItem("My Network", R.drawable.ic_network, "my_network")
     object Post: BottomNavItem("Post", R.drawable.ic_add_post, "post")
--- app/src/main/java/com/example/jetpackexample/navigation_impl/NavigationExample.kt
+++ app/src/main/java/com/example/jetpackexample/navigation_impl/NavigationExample.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -19,7 +18,6 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.jetpackexample.R
 import com.example.jetpackexample.navigation_impl.home.ui.HomeUi
 import com.example.jetpackexample.navigation_impl.job.ui.JobUi
 import com.example.jetpackexample.navigation_impl.mynetwork.ui.MyNetworkUi
@@ -49,9 +47,29 @@ class NavigationExample : ComponentActivity() {
 
 }
 
+/**
+ * Here we've created a navigation graph which is equivalent to xml navigation>nav_graph.xml file for a bottom sheet fragment
+ */
 @Composable
 fun NavigationGraph(navController: NavHostController) {
+    /**
+     * To use this navHost and all the navigation component in compose.
+     * Add the below dependency into your build.gradle(:app)
+     * implementation "androidx.navigation:navigation-compose:2.6.0-beta01"
+     */
+
+    /**
+     * NavHost is a composable function which accepts navController along with other arguments as well, but for this tutorial,
+     * we'll stick with 2 parameters i.e. navController along with startDestination (Start destination is same as what we have in xml navigation_graph.xml)
+     */
     NavHost(navController = navController, startDestination = BottomNavItem.Home.screenRoute) {
+        /**
+         * Assume composable as a <fragment/> tag, since we don't have any fragment concept in jetpack compose hence, we'll be using composable extension to create those and add screens under it
+         */
+
+        /**
+         * This extension function excepts you to pass route name along with Screen
+         */
         composable(BottomNavItem.Home.screenRoute) {
             HomeUi.HomeScreen()
         }
@@ -70,8 +88,14 @@ fun NavigationGraph(navController: NavHostController) {
     }
 }
 
+/**
+ * Here, we're setting the BottomNavigation and adding a parameter for navController of type NavController
+ */
 @Composable
 fun BottomNavigation(navController: NavController) {
+    /**
+     * It will have the list of item i.e. all the screen data
+     */
     val items = listOf(
         BottomNavItem.Home,
         BottomNavItem.MyNetwork,
@@ -79,10 +103,16 @@ fun BottomNavigation(navController: NavController) {
         BottomNavItem.Notification,
         BottomNavItem.Job
     )
-    androidx.compose.material.BottomNavigation(
+    /**
+     * Added BottomNavigation which comes with compose
+     */
+    BottomNavigation(
         backgroundColor = Color.LightGray,
         contentColor = Color.Black
     ) {
+        /**
+         * For handling navigation and all
+         */
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
